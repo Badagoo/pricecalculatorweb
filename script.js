23e0a314c510e45ef0021781be783bdd3ed4f1f4
Badagoo <rehbar.saluja@gmail.com>
2023-11-08 22:04:10 +1100

    idiot proofing

--- script.js
+++ script.js
@@ -24,30 +24,29 @@ function calculate() {
         fetch("https://raw.githubusercontent.com/Badagoo/pricecalculatorweb/main/prices/Domestic.json")
             .then(response => response.json())
             .then(data => {
-                let price = data[height][0][width];
-                let special = price * 1.2 + 427.5; special = Math.round(special * 100) / 100;
-                rrp = special * 1.25; rrp = Math.round(rrp * 100) / 100;
-                let p = document.createElement("p"); p.id = "shutter" + num;
-                p.innerHTML = num + ": $" + special + " (Special) | $" + rrp + " (RRP)";
-                document.getElementById("prices").appendChild(p);
-                specialtotal[num] = special; rrptotal[num] = rrp;
-                for (let i = 1; i < num; i++) {
-                    console.log(i,num)
-                    total += specialtotal[i];
-                    totalrrp += rrptotal[i];
+                try{
+                    let price = data[height][0][width]
+                    let special = price * 1.2 + 427.5; special = Math.round(special * 100) / 100;
+                    rrp = special * 1.25; rrp = Math.round(rrp * 100) / 100;
+                    let p = document.createElement("p"); p.id = "shutter" + num;
+                    p.innerHTML = num + ": $" + special + " (Special) | $" + rrp + " (RRP)";
+                    document.getElementById("prices").appendChild(p);
+                    specialtotal[num] = special; rrptotal[num] = rrp;
+                    for (let i = 1; i < num; i++) {total += specialtotal[i]; totalrrp += rrptotal[i];}
+                    total += special; totalrrp += rrp;
+                    num++;
+                    total = Math.round(total * 100) / 100;
+                    totalrrp = Math.round(totalrrp * 100) / 100;
+                    document.getElementById("total").innerHTML = "Total: $" + total + " (Special) | $" + totalrrp + " (RRP)";
                 }
-                total += special; totalrrp += rrp;
-                num++;
-                total = Math.round(total * 100) / 100;
-                totalrrp = Math.round(totalrrp * 100) / 100;
-                document.getElementById("total").innerHTML = "Total: $" + total + " (Special) | $" + totalrrp + " (RRP)";
-                console.log(total, totalrrp, num);
+                catch(err) {alert("Invalid height and width!"); return;}
             });
     }
     else if (type==2) {
         fetch("https://raw.githubusercontent.com/Badagoo/pricecalculatorweb/main/prices/55mm.json")
             .then(response => response.json())
             .then(data => {
+                try {
                 let price = data[height][0][width];
                 let special = (price * 1.1 * 1.3 + 571) * 1.1; special = Math.round(special * 100) / 100;
                 rrp = special * 1.25; rrp = Math.round(rrp * 100) / 100;
@@ -56,23 +55,21 @@ function calculate() {
                 p.innerHTML = num + ": $" + special + " (Special) | $" + rrp + " (RRP)";
                 document.getElementById("prices").appendChild(p);
                 specialtotal[num] = special; rrptotal[num] = rrp;
-                for (let i = 1; i < num; i++) {
-                    console.log(i,num)
-                    total += specialtotal[i];
-                    totalrrp += rrptotal[i];
-                }
+                for (let i = 1; i < num; i++) {total += specialtotal[i]; totalrrp += rrptotal[i];}
                 total += special; totalrrp += rrp;
                 num++;
                 total = Math.round(total * 100) / 100;
                 totalrrp = Math.round(totalrrp * 100) / 100;
                 document.getElementById("total").innerHTML = "Total: $" + total + " (Special) | $" + totalrrp + " (RRP)";
-                console.log(total, totalrrp, num);
+                }
+                catch(err) {alert("Invalid height and width!"); return;}
             });
     }
     else if (type==3) {
         fetch("https://raw.githubusercontent.com/Badagoo/pricecalculatorweb/main/prices/77mm.json")
             .then(response => response.json())
             .then(data => {
+                try {
                 let price = data[height][0][width];
                 let special = (price * 1.1 * 1.3 + 571) * 1.1; special = Math.round(special * 100) / 100;
                 rrp = special * 1.25; rrp = Math.round(rrp * 100) / 100;
@@ -81,23 +78,21 @@ function calculate() {
                 p.innerHTML = num + ": $" + special + " (Special) | $" + rrp + " (RRP)";
                 document.getElementById("prices").appendChild(p);
                 specialtotal[num] = special; rrptotal[num] = rrp;
-                for (let i = 1; i < num; i++) {
-                    console.log(i,num)
-                    total += specialtotal[i];
-                    totalrrp += rrptotal[i];
-                }
+                for (let i = 1; i < num; i++) {total += specialtotal[i]; totalrrp += rrptotal[i];}
                 total += special; totalrrp += rrp;
                 num++;
                 total = Math.round(total * 100) / 100;
                 totalrrp = Math.round(totalrrp * 100) / 100;
                 document.getElementById("total").innerHTML = "Total: $" + total + " (Special) | $" + totalrrp + " (RRP)";
-                console.log(total, totalrrp, num);
+                }
+                catch(err) {alert("Invalid height and width!"); return;}
             });
     }
     else if (type==4) {
         fetch("https://raw.githubusercontent.com/Badagoo/pricecalculatorweb/main/prices/EasyView.json")
             .then(response => response.json())
             .then(data => {
+                try {
                 let price = data[height][0][width];
                 let special = (price * 1.1 * 1.3 + 571) * 1.1; special = Math.round(special * 100) / 100;
                 rrp = special * 1.25; rrp = Math.round(rrp * 100) / 100;
@@ -106,23 +101,21 @@ function calculate() {
                 p.innerHTML = num + ": $" + special + " (Special) | $" + rrp + " (RRP)";
                 document.getElementById("prices").appendChild(p);
                 specialtotal[num] = special; rrptotal[num] = rrp;
-                for (let i = 1; i < num; i++) {
-                    console.log(i,num)
-                    total += specialtotal[i];
-                    totalrrp += rrptotal[i];
-                }
+                for (let i = 1; i < num; i++) {total += specialtotal[i]; totalrrp += rrptotal[i];}
                 total += special; totalrrp += rrp;
                 num++;
                 total = Math.round(total * 100) / 100;
                 totalrrp = Math.round(totalrrp * 100) / 100;
                 document.getElementById("total").innerHTML = "Total: $" + total + " (Special) | $" + totalrrp + " (RRP)";
-                console.log(total, totalrrp, num);
+                }
+                catch(err) {alert("Invalid height and width!"); return;}
             });
     }
     else if (type==5) {
         fetch("https://raw.githubusercontent.com/Badagoo/pricecalculatorweb/main/prices/36mm.json")
             .then(response => response.json())
             .then(data => {
+                try {
                 let price = data[height][0][width];
                 let special = (price * 1.1 * 1.3 + 571) * 1.1; special = Math.round(special * 100) / 100;
                 rrp = special * 1.25; rrp = Math.round(rrp * 100) / 100;
@@ -131,25 +124,24 @@ function calculate() {
                 p.innerHTML = num + ": $" + special + " (Special) | $" + rrp + " (RRP)";
                 document.getElementById("prices").appendChild(p);
                 specialtotal[num] = special; rrptotal[num] = rrp;
-                for (let i = 1; i < num; i++) {
-                    console.log(i,num)
-                    total += specialtotal[i];
-                    totalrrp += rrptotal[i];
-                }
+                for (let i = 1; i < num; i++) {total += specialtotal[i]; totalrrp += rrptotal[i];}
                 total += special; totalrrp += rrp;
                 num++;
                 total = Math.round(total * 100) / 100;
                 totalrrp = Math.round(totalrrp * 100) / 100;
                 document.getElementById("total").innerHTML = "Total: $" + total + " (Special) | $" + totalrrp + " (RRP)";
-                console.log(total, totalrrp, num);
+                }
+                catch(err) {alert("Invalid height and width!"); return;}
             });
     }
     if (num==1) {
-        let clear = document.createElement("button");
-        clear.id = "clear";
-        clear.innerHTML = "Clear";
-        clear.onclick = function() {clearPrices()};
-        document.getElementById("prices").appendChild(clear);
+        if (document.getElementById("clear") == null) {
+            let clear = document.createElement("button");
+            clear.id = "clear";
+            clear.innerHTML = "Clear";
+            clear.onclick = function() {clearPrices()};
+            document.getElementById("prices").appendChild(clear);
+        }
     }
     document.getElementById("h").value = "";
     document.getElementById("w").value = "";
