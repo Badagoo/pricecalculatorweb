e6877cf75316303aca53aaba2c7a82e3a367915d
Badagoo <rehbar.saluja@gmail.com>
2023-11-18 18:00:01 +1100

    yes
    yes

--- script.js
+++ script.js
@@ -1,27 +1,63 @@
+// import the emailjs module
+
 let num = 1;
-let specialtotal = {};
-let rrptotal = {};
+let shutterIdentity = {};
+
+let total = 0;
+let totalrrp = 0;
+
+function specialToggle() {
+    document.getElementById("total").innerHTML = "Total: $" + total;
+    for (let i = 1; i < num; i++) {document.getElementById("shutter" + i).children[0].innerHTML = shutterIdentity[i][3] + ": $" + shutterIdentity[i][4];}
+
+}
+function rrpToggle() {
+    document.getElementById("total").innerHTML = "Total: $" + totalrrp;
+    for (let i = 1; i < num; i++) {document.getElementById("shutter" + i).children[0].innerHTML = shutterIdentity[i][3] + ": $" + shutterIdentity[i][5];}
+}
 
 function clearSpec(snum) {
     if (confirm("Are you sure you want to clear this price?")) {
-        console.log(snum);
         document.getElementById("shutter" + snum).remove();
-        // replace snum with snum+1, snum+2, etc. until num in specialtotal and rrptotal
-        for (let i = +snum; i < num; i++) {
-            specialtotal[i] = specialtotal[i+1];
-            rrptotal[i] = rrptotal[i+1];
-        }  
-        delete specialtotal[num];
-        delete rrptotal[num];
+        delete shutterIdentity[snum];
+        // rewrite the shutterIdentity array
+        let temp = {};
+        let count = 1;
+        for (let i = 1; i < num; i++) {
+            if (shutterIdentity[i] != undefined) {
+                temp[count] = shutterIdentity[i];
+                count++;
+            }
+        }
+        shutterIdentity = temp;
+        // delete all the divs in prices and rewrite them
+        document.getElementById("prices").innerHTML = "";
+
+        for (let i = 1; i < (num-1); i++) {
+            let divelement = document.createElement("div"); divelement.id = "shutter" + i; divelement.style.display = "flex"; divelement.style.alignItems = "center";
+            let name = shutterIdentity[i][3];
+            let p = document.createElement("p"); p.innerHTML = name + ": $" + shutterIdentity[i][4];
+
+            let clear = document.createElement("button"); clear.innerHTML = "Clear"; clear.id = "clear" + i;
+            clear.onclick = function() {clearSpec(this.id.slice(5))};
+            clear.style.width = "100px"; clear.style.height = "30px"; clear.style.marginLeft = "10px";
+            clear.style.fontSize = "15px"; clear.style.borderRadius = "5px"; clear.style.backgroundColor = "red";
+            clear.style.color = "white"; clear.style.border = "none"; clear.style.outline = "none";
+
+            divelement.appendChild(p);
+            divelement.appendChild(clear);
+            document.getElementById("prices").appendChild(divelement);
+        }
         num--;
-        console.log(specialtotal);
-        // update total
-        let total = 0;
-        let totalrrp = 0;
-        for (let i = 1; i < num; i++) {total += specialtotal[i]; totalrrp += rrptotal[i];}
+
+
+        total = 0;
+        totalrrp = 0;
+        for (let i = 1; i < num; i++) {total += shutterIdentity[i][4]; totalrrp += shutterIdentity[i][5];}
         total = Math.round(total * 100) / 100;
         totalrrp = Math.round(totalrrp * 100) / 100;
-        document.getElementById("total").innerHTML = "Total: $" + total + " (Special) | $" + totalrrp + " (RRP)";
+        console.log(total, totalrrp)
+        document.getElementById("total").innerHTML = "Total: $" + total;
     }
 }
 
@@ -30,8 +66,22 @@ function calculate() {
     let etsd = document.getElementById("w").value;
     const type = document.getElementById("t").value;
     const sname = document.getElementById("sname").value;
-    let total = 0;
-    let totalrrp = 0;
+
+    const wfit = document.getElementById("wfit").value;
+    const hfit = document.getElementById("hfit").value;
+
+    let actualhfit = "";
+    let actualwfit = "";
+
+    if (hfit == 0) {actualhfit = "+box"}
+    else if (hfit == 1) {actualhfit = "-box"}
+
+    if (wfit == 0) {actualwfit = "++"}
+    else if (wfit == 1) {actualwfit = "--"}
+    else if (wfit == 2) {actualwfit = "+-"}
+
+    let initialHeight = trick;
+    let initialWidth = etsd;
 
     if (type==1) {trick = +trick + 205; etsd = +etsd + 120} // Domestic
     else if (type==2) {trick = +trick + 250; etsd = +etsd + 140} // 55mm
@@ -48,6 +98,8 @@ function calculate() {
         .then(response => response.json())
         .then(data => {
             try{
+                total = 0
+                totalrrp = 0
                 let special = 0;
                 let price = data[trick][0][etsd]
                 if (type != 1) {special = (price * 1.1 * 1.3 + 571) * 1.1; special = Math.round(special * 100) / 100}
@@ -56,7 +108,7 @@ function calculate() {
 
                 let divelement = document.createElement("div"); divelement.id = "shutter" + num;
                 divelement.style.display = "flex"; divelement.style.alignItems = "center";
-                let p = document.createElement("p"); p.innerHTML = sname + ": $" + special + " (Special) | $" + rrp + " (RRP)";
+                let p = document.createElement("p"); p.innerHTML = sname + ": $" + special;
 
                 let clear = document.createElement("button"); clear.innerHTML = "Clear"; clear.id = "clear" + num;
                 clear.onclick = function() {clearSpec(this.id.slice(5))};
@@ -64,16 +116,18 @@ function calculate() {
                 clear.style.fontSize = "15px"; clear.style.borderRadius = "5px"; clear.style.backgroundColor = "red";
                 clear.style.color = "white"; clear.style.border = "none"; clear.style.outline = "none";
 
-
                 divelement.appendChild(p);
                 divelement.appendChild(clear);
                 document.getElementById("prices").appendChild(divelement);
-                specialtotal[num] = special; rrptotal[num] = rrp;
-                for (let i = 1; i < num; i++) {total += specialtotal[i]; totalrrp += rrptotal[i];}
+
+
+                shutterIdentity[num] = [trick, etsd, type, sname, special, rrp, initialHeight, initialWidth, actualhfit, actualwfit];
+
+                for (let i = 1; i < num; i++) {total += shutterIdentity[i][4]; totalrrp += shutterIdentity[i][5];}
                 total += special; totalrrp += rrp; num++;
                 total = Math.round(total * 100) / 100;
                 totalrrp = Math.round(totalrrp * 100) / 100;
-                document.getElementById("total").innerHTML = "Total: $" + total + " (Special) | $" + totalrrp + " (RRP)";
+                document.getElementById("total").innerHTML = "Total: $" + total;
             }
             catch(err) {alert("Invalid height and width!"); return;}
         });
@@ -85,16 +139,58 @@ function calculate() {
             clear.onclick = function() {clearPrices()};
             document.getElementById("prices").appendChild(clear);
         }
+        if (document.getElementById("rrpButton") == null) {
+
+            let specialButton = document.createElement("button");
+            specialButton.id = "specialButton";
+            specialButton.innerHTML = "Special";
+            specialButton.onclick = function() {specialToggle()};
+            document.getElementById("priceType").appendChild(specialButton);
+
+            let rrpButton = document.createElement("button");
+            rrpButton.id = "rrpButton";
+            rrpButton.innerHTML = "RRP";
+            rrpButton.onclick = function() {rrpToggle()};
+            document.getElementById("priceType").appendChild(rrpButton);
+        }
     }
     document.getElementById("h").value = "";
     document.getElementById("w").value = "";
     document.getElementById("sname").value = "";
 
 }
+
 function clearPrices() {
     if (confirm("Are you sure you want to clear the prices?")) {
         document.getElementById("prices").innerHTML = "";
         num = 1;
         document.getElementById("total").innerHTML = "";
+        document.getElementById("specialButton").remove();
+        document.getElementById("rrpButton").remove();
     }
+}
+
+function testing() {
+    let message = "";
+    for (i = 1; i < num; i++) {
+        message += shutterIdentity[i][3] + ": " + shutterIdentity[i][6] + " height (fit: " + shutterIdentity[i][8] + ") | " + shutterIdentity[i][7] + " width (fit: " + shutterIdentity[i][9] + ")\n"
+    }
+    message += "\nTotal: $" + total + " (special)"
+    console.log(message);
+}
+
+function sendEmail() {
+    let message = "";
+    for (i = 1; i < num; i++) {
+        message += shutterIdentity[i][3] + ": " + shutterIdentity[i][6] + " height (fit: " + shutterIdentity[i][8] + ") | " + shutterIdentity[i][7] + " width (fit: " + shutterIdentity[i][9] + ")\n"
+    }
+    message += "\nTotal: $" + total + " (special)"
+    emailjs.send("service_d98mljk","template_n8zlzx6",{
+        clName: document.getElementById("clName").value,
+        clAddress: document.getElementById("clName").value,
+        clPhone: document.getElementById("clName").value,
+        clEmail: document.getElementById("clName").value,
+        message: message,
+        clNotes: document.getElementById("clName").value,
+        });
 }
